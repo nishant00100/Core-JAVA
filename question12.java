39e7f21e6f3d7aa3af2c9284d0c6907f4b2f582c
Nishant Chauhan <164226990+nishant00100@users.noreply.github.com>
2025-08-19 11:07:41 +0530

    Add files via upload

---
+++ question12.java
@@ -0,0 +1,14 @@
+class User{
+    final int x;
+    User(){
+        this.x = 23;
+    }
+
+}
+
+public class question12 {
+    public static void main(String[] args) {
+        User obj = new User();
+        System.out.println(obj.x);
+    }
+}

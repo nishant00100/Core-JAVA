8def7cb8b1ce87008e29ef0b9564f782ebd6582b
Nishant Chauhan <164226990+nishant00100@users.noreply.github.com>
2025-08-19 11:08:31 +0530

    Add files via upload

---
+++ question16.java
@@ -0,0 +1,14 @@
+
+class before{
+    void display(){
+        int x=10;
+        System.out.println(x);
+    }
+}
+
+public class question16 {
+    public static void main(String[] args) {
+        before obj = new before();
+        obj.display();
+    }
+}
